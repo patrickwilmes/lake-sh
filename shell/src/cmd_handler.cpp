f0bb1159009e320cb0d269f1eaf180c052c13352
Patrick Wilmes <patrick.wilmes@bit-lake.com>
2021-04-11 20:20:20 +0200

    basic impl of pwd

--- shell/src/cmd_handler.cpp
+++ shell/src/cmd_handler.cpp
@@ -1,12 +1,14 @@
 #include "cmd_handler.hpp"
+#include "usr.hpp"
 #include <string>
+#include <iostream>
 
 bool is_own_cmd(lsh::assembler::cmd *cmd);
 void handle_own_cmd(lsh::assembler::cmd *cmd);
 void handle_cmd(lsh::assembler::cmd *cmd);
 
 void lsh::cmd::handle_commands(std::vector<lsh::assembler::cmd *> cmds) {
-    std::for_each(cmds.begin(), cmds.end(), [](lsh::assembler::cmd *cmd){
+    std::for_each(cmds.begin(), cmds.end(), [](lsh::assembler::cmd *cmd) {
         if (is_own_cmd(cmd)) {
             handle_own_cmd(cmd);
         } else {
@@ -17,15 +19,17 @@ void lsh::cmd::handle_commands(std::vector<lsh::assembler::cmd *> cmds) {
 
 bool is_own_cmd(lsh::assembler::cmd *cmd) {
     std::vector<std::string> own_cmds = {
-            "pwd", "cd"
-    };
+            "pwd", "cd"};
     return std::find(own_cmds.begin(), own_cmds.end(), cmd->name) != own_cmds.end();
 }
 
 void handle_own_cmd(lsh::assembler::cmd *cmd) {
-
+    if (cmd->name == "pwd") {
+        char * wd = lsh::usr::current_wd();
+        std::cout << wd << '\n' << std::flush;
+        free(wd);
+    }
 }
 
 void handle_cmd(lsh::assembler::cmd *cmd) {
-
 }
