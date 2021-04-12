39a64a9c40a81a1fc6a426a45b52d9e0ceb440a5
Patrick Wilmes <patrick.wilmes@bit-lake.com>
2021-04-12 20:23:54 +0200

    navigating forward

--- shell/src/cmd/cmd_cd.cpp
+++ shell/src/cmd/cmd_cd.cpp
@@ -1,15 +1,18 @@
 #include "cmd.hpp"
 #include "usr.hpp"
-#include <climits>
-#include <cstring>
 #include <string>
 #include <unistd.h>
 
 std::string resolve_path(std::string &origin_path) {
     if (origin_path == "..") {
-        auto new_path = origin_path.substr(0, origin_path.rfind('/') - 1);
-        chdir(new_path.c_str());
+        return origin_path.substr(0, origin_path.rfind('/') - 1);
+    } else if ((origin_path[0] != '.' && origin_path[0] != '/') || (origin_path[0] == '.' && origin_path[1] == '/')) {
+        char *wd = lsh::usr::current_wd();
+        std::string wds(wd);
+        free(wd);
+        return wds + "/" + origin_path;
     }
+    return "";
 }
 
 void lsh::cmd::handle_cd(std::shared_ptr<lsh::assembler::cmd> cmd) {
@@ -18,6 +21,6 @@ void lsh::cmd::handle_cd(std::shared_ptr<lsh::assembler::cmd> cmd) {
         chdir(usr_home);
         free(usr_home);
     } else {
-        resolve_path(cmd->args[0]);
+        chdir(resolve_path(cmd->args[0]).c_str());
     }
 }
