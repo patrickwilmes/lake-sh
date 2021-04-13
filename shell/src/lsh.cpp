a0d4433d1f8dfcc47fefa5d90be5f44d54599836
Patrick Wilmes <patrick.wilmes@bit-lake.com>
2021-04-13 14:52:55 +0200

    rendering basic prompt using ncurses

--- shell/src/lsh.cpp
+++ shell/src/lsh.cpp
@@ -10,6 +10,8 @@
 #include <string>
 #include <vector>
 
+using namespace lsh;
+
 static const std::string EXIT_KWD = "exit";
 
 typedef struct {
@@ -21,21 +23,73 @@ render_info *render_info_new();
 void render_info_free(render_info *info);
 std::vector<std::shared_ptr<lsh::assembler::cmd>> process_input(std::string &line);
 
-void lsh::run() {
-    display_prompt();
-    for (std::string line; std::getline(std::cin, line);) {
-        if (line == EXIT_KWD) {
-            exit(EXIT_SUCCESS);
-        }
-        auto cmds = process_input(line);
-        lsh::cmd::handle_commands(cmds);
+lake_shell::lake_shell() {
+    win = initscr();
+    clear();
+    cbreak();
+    noecho();
+    keypad(stdscr, true);
+}
+
+lake_shell::~lake_shell() {
+    endwin();
+}
+
+void lake_shell::run() {
+    while (running) {
+        char *inbuf = (char *) malloc(sizeof(char) * 2048);
+        bool collecting_input = true;
         display_prompt();
+        int x, y;
+        getyx(win, y, x);
+        while (collecting_input) {
+            int c = getch();
+            switch (c) {
+                case KEY_DOWN:
+                    mvwinstr(win, y, x, inbuf);
+                    collecting_input = false;
+                    clear();
+                    break;
+                case KEY_ENTER:
+                case 10:
+                    mvwinstr(win, y, x, inbuf);
+                    collecting_input = false;
+                    clear();
+                    display_prompt();
+                    break;
+                case KEY_BACKSPACE:
+                case KEY_DC:
+                case 127:
+                    int cx, cy;
+                    getyx(win, cy, cx);
+                    mvdelch(cy, cx - 1);
+                    break;
+                default:
+                    echochar(c);
+                    break;
+            }
+        }
+        std::string command_input(inbuf);
+        free(inbuf);
+        echo();
+        refresh();
+        move(y + 1, 0);
+        reset_shell_mode();
+        auto cmds = process_input(command_input);
+        lsh::cmd::handle_commands(cmds);
+        reset_prog_mode();
+        noecho();
+        refresh();
     }
 }
 
-void lsh::display_prompt() {
+void lake_shell::display_prompt() {
     render_info *info = render_info_new();
-    std::cout << info->name << "@" << info->working_dir << " >> ";
+    echo();
+    curs_set(0);
+    addstr(info->name);
+    addstr(" >> ");
+    noecho();
     render_info_free(info);
 }
 
--- shell/src/lsh.hpp
+++ shell/src/lsh.hpp
@@ -1,9 +1,20 @@
 #ifndef LAKE_SH_LSH_H
 #define LAKE_SH_LSH_H
 
+#include <ncurses.h>
+
 namespace lsh {
-    void run();
-    void display_prompt();
+    class lake_shell final {
+    public:
+	lake_shell();
+	~lake_shell();
+	void run();
+    private:
+	void display_prompt();
+    private:
+	WINDOW *win = nullptr;
+	bool running = true;
+    };
 }
 
 #endif//LAKE_SH_LSH_H
