84e6259f09d7d79b7f5fb8900ca6e59e39c762b1
Patrick Wilmes <patrick.wilmes@bit-lake.com>
2021-04-19 09:36:20 +0200

    revert

--- shell/Executor.cpp
+++ shell/Executor.cpp
@@ -86,30 +86,29 @@ std::string LakeShell::Executor::execute()
     if (pid < 0) {
         throw std::runtime_error("failed to fork process");
     } else if (pid == 0) {
-//        if (m_cmd->get_name() != "clear") {
-//            close(fd[0]);
-//            dup2(fd[1], 1);
-//            dup2(fd[1], 2);
-//            close(fd[1]);
-//        }
-//        execvp(c, args);
-        Term::guarded_call(c, args);
+        if (m_cmd->get_name() != "clear") {
+            close(fd[0]);
+            dup2(fd[1], 1);
+            dup2(fd[1], 2);
+            close(fd[1]);
+        }
+        execvp(c, args);
         throw std::runtime_error("failed to execute Command!");
     } else {
         do {
             p = wait(&child_status);
         } while (p != pid);
-//        if (m_cmd->get_name() != "clear") {
-//            char buffer[2048];
-//            for (char & b : buffer) {
-//                b = 0;
-//            }
-//            close(fd[1]);
-//            while (read(fd[0], buffer, sizeof(buffer)) != 0)
-//                ;
-//            std::string data(buffer);
-//            return data;
-//        }
+        if (m_cmd->get_name() != "clear") {
+            char buffer[2048];
+            for (char & b : buffer) {
+                b = 0;
+            }
+            close(fd[1]);
+            while (read(fd[0], buffer, sizeof(buffer)) != 0)
+                ;
+            std::string data(buffer);
+            return data;
+        }
     }
     return std::string();
 }
