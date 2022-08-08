6035ba507f206eab5d767fb20f2b824f01105bb1
Patrick Wilmes <patrick.wilmes@bit-lake.com>
2022-08-08 12:55:04 +0200

    Replacing home directory by ~
    Now the home directory in the prompt is replaced by ~, in order
    to keep the prompt nice and short.

--- src/Prompt.cpp
+++ src/Prompt.cpp
@@ -2,6 +2,7 @@
 #include "TextRendering.hpp" 
 
 #include <utility>
+#include <regex>
 using namespace LakeShell;
 
 Prompt::Prompt(std::shared_ptr<ShellContext> context)
@@ -14,7 +15,7 @@ std::string Prompt::render_prompt()
     std::string prompt = m_shell_context->get_username();
     auto user_home = m_shell_context->get_user_home();
     auto wd = m_shell_context->get_working_dir();
-    prompt += " @ " + m_shell_context->get_working_dir();
+    prompt += " @ " + std::regex_replace(m_shell_context->get_working_dir(), std::regex(user_home), "~");
     if (m_shell_context->is_git_dir()) {
         prompt += spaced(red("git"));
     }
